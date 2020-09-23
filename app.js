09cc9de67fbec513f8029d41f7c434c662b42373
Jvinicius06 <30390323+Jvinicius06@users.noreply.github.com>
2020-09-23 11:42:21 -0300

    erro Redirect HTTPS

--- app.js
+++ app.js
@@ -18,10 +18,9 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== 'development') {
   app.use((req, res, next) => {
     if ((req.headers['x-forwarded-proto'] || '').endsWith('http')) {
-      console.log('redirect');
       res.redirect(`https://${req.headers.host}${req.url}`);
     } else {
       next();
