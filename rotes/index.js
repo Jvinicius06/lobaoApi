3765a5350b0d4ddc5517c937ddd80ee46ce130f1
Jvinicius06 <jvinicius06@gmail.com>
2020-10-01 20:38:04 -0300

    Path dist

--- rotes/index.js
+++ rotes/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = (app) => {
   if (process.env.NODE_ENV !== 'development') {
     app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, '..', 'webpack', 'dist', 'index.html'));
+      res.sendFile(path.join(__dirname, 'webpack', 'dist', 'index.html'));
     });
   }
 }
