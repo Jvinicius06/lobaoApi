91f7a0e5a14c52725f449aaf5f59201293ab981f
Jvinicius06 <jvinicius06@gmail.com>
2020-11-04 23:25:14 -0300

    colocado /count a frente do contador para não contar como conexão

--- middleware/cont.js
+++ middleware/cont.js
@@ -6,18 +6,19 @@ const db = low(adapter)
 
 
 module.exports = (app) => {
+  app.get('/count', async (req, res) => {
+    if (req.query.delete == 'true') {
+      await db.update('count', n => 0)
+      .write()
+    }
+    res.status(200).send(db.value());
+  });
+  
   app.use(async (req, res, next) => {
     await db.update('count', n => n + 1)
             .write()
     next();
   });
 
-  app.get('/count', async (req, res) => {
-    if (req.query.delete == 'true') {
-      await db.update('count', n => 0)
-              .write()
-    }
-    res.status(200).send(db.value());
-  });
 
 }
